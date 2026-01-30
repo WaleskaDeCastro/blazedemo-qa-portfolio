6c349dddfe2b0eb166b4b879c2e56627e4c7e738
Waleska de Castro <108630891+WaleskaDeCastro@users.noreply.github.com>
2026-01-30 11:06:19 -0300

    adiciona mapa mental do BlazeDemo
    Adição do mapa mental do BlazeDemo, contendo análise do site através de perguntas 5W2H, funcionalidades principais e fluxos de navegação. Arquivo faz parte do portfólio de QA Júnior.

---
+++ 01-Mapa-Mental/02_MapaMental_5W2H_BlazeDemo.mm
@@ -0,0 +1,90 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1769132730981" ID="ID_929031335" MODIFIED="1769619996079" TEXT="5W2H">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="edit"/>
+<node CREATED="1769133000576" ID="ID_287913469" MODIFIED="1769696956836" POSITION="right" TEXT="Site da companhia a&#xe9;rea  BlazeDemo">
+<font NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1769133088889" ID="ID_690233422" MODIFIED="1769696958451" TEXT="What (O qu&#xea; ser&#xe1; testado?)">
+<node CREATED="1769133244220" ID="ID_18516788" MODIFIED="1769557335768" TEXT="Funcionalidades principais: ">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1769133383568" ID="ID_1098352118" MODIFIED="1769696989144" TEXT="Inser&#xe7;&#xe3;o de local de partida e destino"/>
+<node CREATED="1769133462402" ID="ID_1683129819" MODIFIED="1769557335769" TEXT="Sele&#xe7;&#xe3;o e data e hor&#xe1;rio"/>
+<node CREATED="1769133491377" ID="ID_248926647" MODIFIED="1769557335769" TEXT="Preenchimento de dados pessoais "/>
+<node CREATED="1769133516271" ID="ID_715504441" MODIFIED="1769557335769" TEXT="Inser&#xe7;&#xe3;o do n&#xfa;mero do cart&#xe3;o"/>
+<node CREATED="1769133548687" ID="ID_355023722" MODIFIED="1769557335770" TEXT="Confirma&#xe7;&#xe3;o da compra "/>
+<node CREATED="1769133691990" ID="ID_70775250" MODIFIED="1769557335770" TEXT="Elementos de interface:">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1769133780317" ID="ID_253880763" MODIFIED="1769557335770" TEXT="Bot&#xf5;es ( avan&#xe7;ar, voltar, confirmar, pagar)"/>
+<node CREATED="1769133798641" ID="ID_1112686835" MODIFIED="1769557335771" TEXT="Campos de formul&#xe1;rio (obrigat&#xf3;rios, opcionais, formato)"/>
+<node CREATED="1769133831346" ID="ID_444482368" MODIFIED="1769557335771" TEXT="Mensagens de erro e sucesso"/>
+<node CREATED="1769133881010" ID="ID_1404159588" MODIFIED="1769557335771" TEXT="Fluxo entre p&#xe1;ginas: ">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1769133912927" ID="ID_1530040203" MODIFIED="1769557335772" TEXT="Navega&#xe7;&#xe3;o da p&#xe1;gina 1&gt; 2&gt;3&gt;4"/>
+<node CREATED="1769133942512" ID="ID_422757860" MODIFIED="1769557335772" TEXT="Bloqueio de avan&#xe7;o sem dados obrigat&#xf3;rios "/>
+<node CREATED="1769133984428" ID="ID_1065581120" MODIFIED="1769557335772" TEXT="Layout e usabilidade:">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1769134012333" ID="ID_692812458" MODIFIED="1769557335773" TEXT="Campos vis&#xed;veis em diferentes tamanhos de tela "/>
+<node CREATED="1769557136642" ID="ID_687683150" MODIFIED="1769557335773" TEXT="Alinhamento"/>
+<node CREATED="1769557142645" ID="ID_140106870" MODIFIED="1769557335773" TEXT="Textos leg&#xed;veis "/>
+</node>
+<node CREATED="1769134403304" ID="ID_1235995345" MODIFIED="1769696961093" TEXT="Why (Por qu&#xea; testar?)">
+<node CREATED="1769557495928" ID="ID_806107641" MODIFIED="1769696962611" TEXT="Para garantir que: ">
+<icon BUILTIN="yes"/>
+<node CREATED="1769557554443" ID="ID_731734407" MODIFIED="1769557576276" TEXT="O usu&#xe1;rio consiga concluir a compra sem erros"/>
+<node CREATED="1769557577017" ID="ID_96224775" MODIFIED="1769557601283" TEXT="Os dados inseridos sejam validados corretamente"/>
+<node CREATED="1769557602647" ID="ID_1064376733" MODIFIED="1769557614835" TEXT="O valor total seja calculado de forma correta "/>
+<node CREATED="1769557616154" ID="ID_1487651740" MODIFIED="1769557628914" TEXT="O pagamento funcione sem falhas "/>
+</node>
+<node CREATED="1769557734159" ID="ID_1190015596" MODIFIED="1769696963973" TEXT="Para:" VSHIFT="-1">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1769557765799" ID="ID_564220776" MODIFIED="1769557787373" TEXT="Evitar preju&#xed;zos financeiros por bugs"/>
+<node CREATED="1769557788407" ID="ID_1791898092" MODIFIED="1769557808791" TEXT="Reduzir retrabalho da equipe de desenvolvimento"/>
+<node CREATED="1769557810115" ID="ID_1976794571" MODIFIED="1769557820405" TEXT="Aumentar a confian&#xe7;a do cliente no site"/>
+<node CREATED="1769557821267" ID="ID_1224379973" MODIFIED="1769557835716" TEXT="Proteger a imagem da empresa "/>
+</node>
+</node>
+<node CREATED="1769557974966" ID="ID_301956485" MODIFIED="1769696965352" TEXT="Who (Quem est&#xe1; envolvido?)">
+<icon BUILTIN="group"/>
+<node CREATED="1769558009635" ID="ID_1491067075" MODIFIED="1769558057967" TEXT="Usu&#xe1;rios finais (Pessoas comprando passagens)"/>
+<node CREATED="1769558058486" ID="ID_1526543193" MODIFIED="1769558110928" TEXT="Equipe interna (QA; Desenvolvedores; Product Owner ou cliente)"/>
+<node CREATED="1769558111991" ID="ID_1688589261" MODIFIED="1769558166674" TEXT="Sistema externo - se existirem ( Gateway de pagamento; Sistema de e-mail de confirma&#xe7;&#xe3;o)"/>
+</node>
+<node CREATED="1769604661063" ID="ID_1819623857" MODIFIED="1769696966475" TEXT="Where (Onde ser&#xe1; testado?)">
+<icon BUILTIN="xmag"/>
+<node CREATED="1769604918944" ID="ID_141006272" MODIFIED="1769604995683" TEXT="Ambiente: de teste (homologa&#xe7;&#xe3;o); de produ&#xe7;&#xe3;o (ap&#xf3;s libera&#xe7;&#xe3;o)"/>
+<node CREATED="1769604996297" ID="ID_1959259982" MODIFIED="1769605036413" TEXT="Plataformas: Navegador - Chrome, Frefox; Desktop e mobile"/>
+<node CREATED="1769605037083" ID="ID_1233010308" MODIFIED="1769606922038" TEXT="P&#xe1;ginas: 1-Origem/destino; 2-Hor&#xe1;rios; 3-Dados e pagamento e 4-Confirma&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1769605366047" ID="ID_1242116529" MODIFIED="1769696967747" TEXT="When (Quando testar?)">
+<icon BUILTIN="hourglass"/>
+<node CREATED="1769605397424" ID="ID_569990915" MODIFIED="1769605799209" TEXT="Antes da libera&#xe7;&#xe3;o do site">
+<icon BUILTIN="bookmark"/>
+</node>
+<node CREATED="1769605418486" ID="ID_1833404842" MODIFIED="1769605427325" TEXT="A cada nova funcionalidade"/>
+<node CREATED="1769605427844" ID="ID_1295226040" MODIFIED="1769605444941" TEXT="Ap&#xf3;s corre&#xe7;&#xe3;o de bugs (testes de regress&#xe3;o)"/>
+<node CREATED="1769605445526" ID="ID_1221291420" MODIFIED="1769605518764" TEXT="Sempre que houver mudan&#xe7;a no sistema de pagamento"/>
+</node>
+<node CREATED="1769607715740" ID="ID_1442937361" MODIFIED="1769696969765" TEXT="How (Como?)">
+<icon BUILTIN="bell"/>
+<node CREATED="1769605918015" HGAP="189" ID="ID_1980125015" MODIFIED="1769696974667" TEXT="How (Como ser&#xe1; testado?)" VSHIFT="52">
+<icon BUILTIN="list"/>
+<node CREATED="1769605977274" ID="ID_777391865" MODIFIED="1769606891127" TEXT="Tipos de teste: Funcionais; Usabilidade; Valida&#xe7;&#xe3;o de campos"/>
+<node CREATED="1769606477362" ID="ID_1192743475" MODIFIED="1769630432599" TEXT="T&#xe9;cnicas: Casos de testes manuais; Testes explorat&#xf3;rios; Particionamento de equival&#xea;ncia "/>
+<node CREATED="1769606603342" ID="ID_655400124" MODIFIED="1769606647807" TEXT="Ferramentas: Planilha ou sistema de gest&#xe3;o de testes; Navegador"/>
+</node>
+<node CREATED="1769606669445" HGAP="199" ID="ID_1805891925" MODIFIED="1769696975971" STYLE="fork" TEXT="How Much (Quanto custa/ qual o esfor&#xe7;o?)" VSHIFT="16">
+<icon BUILTIN="info"/>
+<node CREATED="1769606743532" ID="ID_767158558" MODIFIED="1769606781193" TEXT="Tempo: 1-2 dias para testes manuais iniciais; Algumas horas para reteste ap&#xf3;s corre&#xe7;&#xf5;es"/>
+<node CREATED="1769606782109" ID="ID_1604883852" MODIFIED="1769606814275" TEXT="Esfor&#xe7;o: 1 QA j&#xfa;nior; Apoio do desenvolvedor para bugs cr&#xed;ticos"/>
+<node CREATED="1769606817807" ID="ID_856508248" MODIFIED="1769606857079" TEXT="Custo: Baixo (se feito antes da produ&#xe7;&#xe3;o; Alto (se bugs forem encontrados ap&#xf3;s clientes usarem o site)"/>
+</node>
+</node>
+</node>
+</node>
+</map>
